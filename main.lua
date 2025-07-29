652de38acdcc3557e4a87ddce1880bbd2ee1fc21
ŁØҚɪ <loki.the.cursed@gmail.com>
2025-07-29 10:00:14 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -41,7 +41,7 @@ local spells =
 
 on_render_menu (function ()
 
-    if not menu.main_tree:push("Necromancer: Base v0.15a") then
+    if not menu.main_tree:push("Necromancer: Base v0.16") then
         return;
     end;
 
